5b977de05c0878909989613da740d759584b65ac
Yasien Essam <yasien.essam99@gmail.com>
2022-12-13 01:11:27 +0200

    add project files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { render } from "react-dom";
+import Pet from "./Pet";
+import { StrictMode } from "react"
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <h1 className="title">Adopt Me!</h1>
+      <SearchParams  name="Luna" animal="dog" breed="Havanese" />
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
